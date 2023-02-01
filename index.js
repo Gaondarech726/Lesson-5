8ed1e918e2c11f27f63106d31a531a6bddb2db04
Gaondarech_726 <anton.honcharenko2202@gmail.com>
2023-02-01 09:47:13 +0200

    Lesson-5

---
+++ index.js
@@ -0,0 +1,16 @@
+const BtnNode = document.querySelector('.js-btn')
+const InputNode = document.querySelector('.js-input')
+
+function isEmpty(str) {
+    if (str.value >= '11')
+        return alert('Введіть число від 1 до 10');
+
+    if (str.value == '') 
+        return alert('Введіть число');
+
+        alert('Дякуємо за відовідь, Ваш голос дуже важливий для нас!')
+    }
+
+BtnNode.addEventListener('click', function() {
+    isEmpty(InputNode)
+})
